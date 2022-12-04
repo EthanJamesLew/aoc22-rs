76e4d10b37e0d49474952c631198b44686cc179e
Ethan Lew <elew@galois.com>
2022-12-04 12:49:39 -0800

    add badge to readme, prevent globbing

--- profile/compare_python.sh
+++ profile/compare_python.sh
@@ -2,14 +2,13 @@
 set -e
 
 # run the rust part
-cargo build --release --example day$1
+cargo build --release --example "day$1"
 echo "\033[0;31mRunning Rust\033[0m"
-perf stat ./target/release/examples/day$1 2> rust.out
+perf stat "./target/release/examples/day$1" 2> rust.out
 
 # run the python part
 echo "\033[0;31mRunning Python\033[0m"
-perf stat python ./python/day$1.py 
-perf stat python ./python/day$1.py 2> python.out
+perf stat python "./python/day$1.py" 2> python.out
 PY_TIME=$(grep -Po "([0-9\.]+) seconds time elapsed" python.out | grep -Po "([0-9\.]+)")
 RS_TIME=$(grep -Po "([0-9\.]+) seconds time elapsed" rust.out | grep -Po "([0-9\.]+)")
 P_MULT=$(echo $PY_TIME / $RS_TIME | bc)
@@ -21,9 +20,9 @@ echo "Rust Time: $RS_TIME seconds"
 echo "Rust is $P_MULT times faster"
 
 # dump results to JSON
-echo "{" > perf_out_day$1.json
-echo "\"python_time\": $PY_TIME," >> perf_out_day$1.json
-echo "\"rust_time\": $RS_TIME" >> perf_out_day$1.json
-echo "}" >> perf_out_day$1.json
+echo "{" > "perf_out_day$1.json"
+echo "\"python_time\": $PY_TIME," >> "perf_out_day$1.json"
+echo "\"rust_time\": $RS_TIME" >> "perf_out_day$1.json"
+echo "}" >> "perf_out_day$1.json"
 # check it
-python ./profile/check_profile_output.py perf_out_day$1.json
+python ./profile/check_profile_output.py "perf_out_day$1.json"
